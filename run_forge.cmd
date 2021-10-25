b3d5df56949f7c8b0f9e6de2dd28a711f83a45b0
Berczi, Sándor <berczi.sandor@gmail.com>
2021-10-25 10:37:34 +0200

    sdf

--- run_forge.cmd
+++ run_forge.cmd
@@ -1,5 +1,10 @@
-%echo on
+%echo off
 
+docker version >NUL 2>NUL || (
+    echo Run docker Desktop first.
+    pause
+    exit /b 1
+)
 
 cd "%~dp0"
 docker-compose -f docker-compose-forge.yml up
--- run_paper.sh
+++ run_paper.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+set -e
+
 SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+docker version >/dev/null|| {
+    echo "Run docker(desktop) first."
+    exit 1
+}
 
 cd "$SCRIPTDIR"
 docker-compose -f docker-compose-paper.yml up
